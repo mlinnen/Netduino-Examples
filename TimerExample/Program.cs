6958f47a93d8fc484e8dbb8f6c8c166cab2b4a20
Mike Linnen <mlinnen@protosystem.net>
2010-09-11 00:33:27 -0400

    Initial checkin of a simple project that uses the timer class

---
+++ TimerExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace TimerExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Kicking off the timer");
+            Debug.Print(DateTime.Now.ToString());
+
+            // Call the OnTimer method after 5 seconds have expired and then repeat every 10 seconds
+            Timer timer = new Timer(new TimerCallback(OnTimer), null, 5000, 10000);
+
+            Debug.Print("Waiting for the timer to expire");
+            Debug.Print(DateTime.Now.ToString());
+            Thread.Sleep(25000);
+
+            Debug.Print("Killing the timer");
+            timer.Dispose();
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+
+
+        public static void OnTimer(object state)
+        {
+            Debug.Print("Timout Triggered");
+            Debug.Print(DateTime.Now.ToString());
+        }
+    }
+}
